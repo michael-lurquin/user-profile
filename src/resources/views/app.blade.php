cc491256b3c288a2e6f09772bb1c2e9e23bdb36c
Michaël Lurquin <michael.lurquin@gmail.com>
2018-03-16 21:13:47 +0100

    Update

--- src/resources/views/app.blade.php
+++ src/resources/views/app.blade.php
@@ -50,7 +50,7 @@
                             </a>
 
                             <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                                <a class="dropdown-item" href="{{ route('profile') }}">{{ __('My Profile') }}</a>
+                                <a class="dropdown-item{{ route::is('profile') ? ' active' : '' }}" href="{{ route('profile') }}">{{ __('My Profile') }}</a>
                                 <a class="dropdown-item" href="{{ route('logout') }}"
                                    onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
