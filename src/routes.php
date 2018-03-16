c72e9a983bedc8a2b734cfed345f161ab957b9b6
Michaël Lurquin <michael.lurquin@gmail.com>
2018-03-16 21:05:10 +0100

    Update

--- src/routes.php
+++ src/routes.php
@@ -1,5 +1,11 @@
 <?php
 
-Route::get('profile', function() {
-    return view('profile')->withAuth(auth()->user());
-})->name('profile')->middleware('auth');
+Route::group(['middleware' => ['web']], function () {
+
+    Route::get('profile', function () {
+
+        return view('profile')->withAuth(auth()->user());
+
+    })->name('profile')->middleware('auth');
+
+});
